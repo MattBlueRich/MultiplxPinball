6f3f1f5322b485acc3aae38a65c6f0f2fb034212
MattBlueRich <S240530@UOS.AC.UK>
2024-04-15 15:46:36 +0100

    Add Mystery Wheel TV Screen Feedback

--- Assets/Scripts/GameScene/WheelUI.cs
+++ Assets/Scripts/GameScene/WheelUI.cs
@@ -21,6 +21,14 @@ public class WheelUI : MonoBehaviour
     public AudioClip wheelMusic;
     public AudioSource managerAudioSource;
 
+    [Header("TV Screen Material")]
+    public Material TVScreenMaterial;
+    public Color luckyColour;
+    public Color unluckyColour;
+    public float strobeDelay;
+    private float strobeCurrentTime;
+    private bool wheelStopped = false;
+
     [Header("Wheel Fate")]
     public bool isLucky;
 
@@ -37,6 +45,8 @@ public class WheelUI : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
 
         angleOfOneSegment = CIRCLE / numberOfSegments;
+
+        TVScreenMaterial.color = luckyColour;
     }
 
     [ContextMenu("Start Wheel")] // By right-clicking this script, this function can be called in the editor.
@@ -71,6 +81,17 @@ public class WheelUI : MonoBehaviour
         We will probably set the status and cooldown of the status in another script!
         */
 
+        wheelStopped = true;
+
+        if (isLucky)
+        {
+            TVScreenMaterial.color = luckyColour;
+        }
+        else
+        {
+            TVScreenMaterial.color = unluckyColour;
+        }
+        
         yield return new WaitForSeconds(1);
         HideWheel();
     }
@@ -92,5 +113,35 @@ public class WheelUI : MonoBehaviour
     public void HideWheel() 
     {
         animator.SetBool("In", false);
+    }
+
+    private void Update()
+    {
+        if (!wheelStopped)
+        {
+            // This if-statement acts as a timer for strobing the fill material.
+            if (strobeCurrentTime > 0)
+            {
+                strobeCurrentTime -= Time.deltaTime; // Decrease current time by Time.deltaTime.
+            }
+            else
+            {
+                strobeCurrentTime = strobeDelay; // Reset current time back to max delay time. 
+                UpdateMaterialColour(); // Change the material colour between colour A and colour B.
+            }
+        }
+    }
+
+    public void UpdateMaterialColour()
+    {
+        // If the material's current colour is colour A...
+        if (TVScreenMaterial.color == luckyColour)
+        {
+            TVScreenMaterial.color = unluckyColour; // Switch to colour B!
+        }
+        else
+        {
+            TVScreenMaterial.color = luckyColour; // Switch to colour A!
+        }
     }
 }
