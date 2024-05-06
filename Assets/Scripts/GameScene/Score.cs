d51486811fbcc74f93a218262139d8c28da7c6cc
MattBlueRich <S240530@UOS.AC.UK>
2024-05-06 08:43:39 +0100

    Increment Score While In Gate & Score Ticking Animation

--- Assets/Scripts/GameScene/Score.cs
+++ Assets/Scripts/GameScene/Score.cs
@@ -8,12 +8,20 @@ using Unity.VisualScripting;
 
 public class Score : MonoBehaviour
 {
+    [Header("Score")]
     public TextMeshProUGUI scoreText;
-    public int score;
-    public static int startingScore = 0;
+    public float score;
+    private float _score;
+    bool tickScore = false;
+    public static float startingScore = 0;
 
+    [Header("Jackpot Letters")]
     public List<char> jackpotLetters = new List<char>() { 'M', 'U', 'L', 'T', 'I', 'P', 'L', 'X', };
     public List<char> jackpotLettersRemaining;
+
+    
+    [Header("Gate Movement Detection")]
+    public PinballScript pinballScript;
 
     private void Awake()
     {
@@ -22,13 +30,36 @@ public class Score : MonoBehaviour
     private void Start()
     {
         score = startingScore;
+        _score = score;
         scoreText.text = score.ToString();
+    }
+
+    private void Update()
+    {
+        // This if-statement ticks the score.
+        if(_score < score)
+        {
+            tickScore = true;
+            _score += Time.deltaTime * 150f;
+            scoreText.text = _score.ToString("F0");
+        }
+        else if(tickScore)
+        {
+            tickScore = false;
+            _score = score;
+            scoreText.text = _score.ToString("F0");
+        }
+
+        // While inside a gate, this if-statement increments the score.
+        if (pinballScript.usingGate)
+        {
+            score += Time.deltaTime * 20;
+        }
     }
 
     public void AddScore(int scoreValue)
     {
-        score += scoreValue;
-        scoreText.text = score.ToString();
+        score += scoreValue;       
     }
 
     // This is called by the RewardScript.cs, when colliding with a jackpot letter target.
