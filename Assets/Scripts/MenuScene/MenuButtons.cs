ae500eec4877302d2b179314bce12247b27fd143
MLLacius <collinsleyton@gmail.com>
2024-05-28 17:02:20 +0100

    Update MenuButtons.cs
    Commented code because I forgot to before when I wrote it

--- Assets/Scripts/MenuScene/MenuButtons.cs
+++ Assets/Scripts/MenuScene/MenuButtons.cs
@@ -10,18 +10,22 @@ public class MenuButton : MonoBehaviour
     public void StartButton()
     {
         SceneManager.LoadScene("UpdateGameScene");
+        //loads the game scene on pressing the start button
     }
     public void LeaderboardButton()
     {
         SceneManager.LoadScene("");
+        //loads the leaderboard scene on pressing the leaderboard button
     }
     public void CreditButton()
     {
         SceneManager.LoadScene("CreditScene");
+        //loads the credits on pressing the credits button
     }
     public void ExitButton()
     {
         Application.Quit();
+        //closes the game on pressing the exit button
     }
 
 }
